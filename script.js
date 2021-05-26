5b91327af82116df2b405d05403e9ea3b384e598
ppreethi25 <72737532+ppreethi25@users.noreply.github.com>
2021-05-26 21:38:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function hamburger() {
+
+    var x = document.getElementById("link");
+    if(x.style.display == "block") {
+        // alert();
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+
+}
+
